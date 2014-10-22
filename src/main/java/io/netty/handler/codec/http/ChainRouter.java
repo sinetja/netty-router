57c86dcb0d7f47448d27dc9bc847d4acbcdc8d18
Ngoc Dao <ngocdaothanh@gmail.com>
2014-10-23 08:17:12 +0900

    Remove unused blank line

--- src/main/java/io/netty/handler/codec/http/ChainRouter.java
+++ src/main/java/io/netty/handler/codec/http/ChainRouter.java
@@ -65,7 +65,6 @@ public class ChainRouter<T extends ChainRouter<T>> extends SimpleChannelInboundH
 
   //----------------------------------------------------------------------------
 
-
   public T group(EventExecutorGroup group) {
     this.group = group;
     return (T) this;
