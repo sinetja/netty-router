e75db363dfe4be39217761224d433a3a4858979d
Ngoc Dao <ngocdaothanh@gmail.com>
2014-10-25 08:53:33 +0900

    http://stackoverflow.com/questions/9655335/method-chaining-how-to-use-getthis-trick-in-case-of-multi-level-inheritance

--- src/main/java/io/netty/handler/codec/http/router/MethodRouter.java
+++ src/main/java/io/netty/handler/codec/http/router/MethodRouter.java
@@ -2,6 +2,7 @@ package io.netty.handler.codec.http.router;
 
 import io.netty.handler.codec.http.HttpMethod;
 
+// http://stackoverflow.com/questions/9655335/method-chaining-how-to-use-getthis-trick-in-case-of-multi-level-inheritance
 public abstract class MethodRouter<T, RouteLike extends MethodRouter<T, RouteLike>> extends jauter.Router<HttpMethod, T, RouteLike> {
   @Override protected HttpMethod CONNECT() { return HttpMethod.CONNECT; }
   @Override protected HttpMethod DELETE()  { return HttpMethod.DELETE ; }
